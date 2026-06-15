5c65e6cd1158c96a821701b72a70139cdaf3d823
TeoMarquez <mycorreo411@gmail.com>
2026-06-15 13:38:13 -0300

    chore(config): migrate configuration to dotenv with safe fallbacks

--- src/config.py
+++ src/config.py
@@ -7,6 +7,9 @@ load_dotenv()
 DATA_DIR = Path("data")
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-AUTHORIZED_USER = int(os.getenv("AUTHORIZED_USER"))
+
+AUTHORIZED_USER = int(os.getenv("AUTHORIZED_USER", "-1")) # Si no se define o se pone -1, va a actuar en modo público
+ADMIN_EMAIL = os.getenv("ADMIN_EMAIL", "admin@localhost.local") #  si no se define, cae en un mail genérico, no recomendada para producción
+
 
 WATCHDOG_FILE = DATA_DIR / "watchdog.json"
